ab3cc0bc7813bda8d6a5188b1db22d9f1f5904ea
Tuan Nguyen <tuan.nat915@gmail.com>
2023-12-20 20:00:46 -0500

    Create structure for frontend and backend

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,7 +1,31 @@
-import './App.css'
+// npm modules
+import { Routes, Route } from "react-router-dom"
+
+// pages
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Profile from "./pages/Profile"
+
+// components
+import NavBar from "./components/NavBar/NavBar"
+
+// styling
+import "./App.css"
+
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <NavBar />
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
+    </>
   )
 }
 
