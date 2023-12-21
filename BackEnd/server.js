1ca9464b4b7b9c2a07e935cfcb0a10aa9a59572d
Tuan Nguyen <tuan.nat915@gmail.com>
2023-12-20 20:08:39 -0500

    Run server for backend

---
+++ BackEnd/server.js
@@ -0,0 +1,21 @@
+import 'dotenv/config'
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
+
+const app = express()
+const PORT = process.env.PORT
+const corsOption = {
+  origin: true
+}
+
+// middleware
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors(corsOption))
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`)
+})
