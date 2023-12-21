8fe9758b0b4f893d19cff80503862792f4ac0b8f
Tuan Nguyen <tuan.nat915@gmail.com>
2023-12-20 21:01:13 -0500

    Create user model

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,20 +1,21 @@
-import 'dotenv/config'
-import express from 'express'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
+import "dotenv/config"
+import express from "express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
 
+// import database
+import "./configs/database.js"
 
 const app = express()
 const PORT = process.env.PORT
 const corsOption = {
-  origin: true
+  origin: true,
 }
 
 // middleware
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors(corsOption))
-
 
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`)
