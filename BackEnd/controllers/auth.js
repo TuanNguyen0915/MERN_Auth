2105a1f8ae7831fb71f1e725fb1065dd17560c66
Tuan Nguyen <tuan.nat915@gmail.com>
2023-12-20 21:49:12 -0500

    Create login controller, login router

--- BackEnd/controllers/auth.js
+++ BackEnd/controllers/auth.js
@@ -1,5 +1,10 @@
 import User from "../models/user.js"
 import bcrypt from "bcryptjs"
+import jwb from "jsonwebtoken"
+
+const generateToken = (user) => {
+  return jwb.sign({ id: user._id }, process.env.JWT_KEY, { expiresIn: "7d" })
+}
 
 // ---------------- Register -------------------
 const register = async (req, res) => {
@@ -20,11 +25,29 @@ const register = async (req, res) => {
       photo: req.body.photo,
     })
     return res.status(201).json({ success: true, message: "Successful to create new account", user: user })
-  } catch (error) {
-    return res.status(500).json({success: false, message: 'Interval server error. Please, try again'})
-  }
+  } catch (error) {}
 }
 
 // ---------------- Login -------------------
 
-export { register }
+const login = async (req, res) => {
+  try {
+    let user = await User.findOne({ email: req.body.email })
+    // check user by email
+    if (!user) {
+      return res.status(404).json({ success: false, message: "Account does not exist." })
+    }
+    // check password matching
+    const isPasswordMatching = await bcrypt.compare(req.body.password, user.password)
+    if (!isPasswordMatching) {
+      return res.status(404).json({ success: false, message: "Password not matching" })
+    }
+    // create token if correct email nad password
+    let token = generateToken(user)
+    return res.status(201).json({ success: true, accessToken: token, user: user })
+  } catch (error) {
+    return res.status(500).json({ success: false, message: "Interval server error. Please, try again" })
+  }
+}
+
+export { register, login }
--- BackEnd/routes/auth.js
+++ BackEnd/routes/auth.js
@@ -4,5 +4,6 @@ import * as authCtrl from "../controllers/auth.js"
 const router = Router()
 
 router.post("/register", authCtrl.register)
+router.post('/login', authCtrl.login)
 
 export { router }
