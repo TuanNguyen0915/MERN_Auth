6e6b8d08a7ae42885f2bcc7da23c5e0a245d3a0f
Tuan Nguyen <tuan.nat915@gmail.com>
2023-12-22 11:34:55 -0500

    complete Register fucntionality

--- FrontEnd/src/pages/Register.jsx
+++ FrontEnd/src/pages/Register.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import * as authServices from "../services/authServices";
 import { toast } from "react-toastify";
-
-const BASE_URL = `${import.meta.env.VITE_SERVER_URL}/auth/register`;
+import { FaLock, FaUser, FaImage } from "react-icons/fa";
+import { IoMailSharp } from "react-icons/io5";
 
 const Register = () => {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
-  const [errMessage, setErrMessage] = useState(null);
+  const errMessage = useRef(null);
   const [formData, setFormData] = useState({
     name: "",
     password: "",
@@ -26,9 +26,10 @@ const Register = () => {
     e.preventDefault();
     setLoading(true);
     try {
-      const data = await authServices.register(formData);
+      const data = await authServices.signUp(formData);
       if (!data.success) {
-        setErrMessage(data.message);
+        errMessage.current = data.message;
+        toast.error(errMessage.current, { position: "top-center" });
       } else {
         toast.success(data.message);
         navigate("/");
@@ -45,56 +46,79 @@ const Register = () => {
         <h1 className="mb-4 text-center text-[28px] font-bold md:text-[48px]">
           Register
         </h1>
-        {/* ERROR MESSAGE */}
-        {errMessage && <h2 className="mb-4 text-red-600">{errMessage}</h2>}
+        {/* ERROR MESSAGE, USING TOAST INSTEAD
+        {errMessage.current && <h2 className="mb-4 text-red-600">{errMessage.current}</h2>} */}
+
         {/* FORM */}
         <form
           className="flex w-full flex-col items-center justify-center gap-4"
           onSubmit={handleSubmit}
         >
-          <input
-            type="text"
-            placeholder="Username"
-            autoComplete="off"
-            required
-            name="name"
-            value={formData.name}
-            className="mt-4 w-full rounded-lg bg-slate-100 p-4 px-8 outline-none focus:outline-textLink"
-            onChange={handleChange}
-          />
-          <input
-            type="email"
-            placeholder="Email"
-            autoComplete="off"
-            required
-            name="email"
-            value={formData.email}
-            className="w-full rounded-lg bg-slate-100 px-8 py-4 outline-none focus:outline-textLink"
-            onChange={handleChange}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            autoComplete="off"
-            required
-            name="password"
-            value={formData.password}
-            className="w-full rounded-lg bg-slate-100 p-4 px-8 outline-none focus:outline-textLink"
-            onChange={handleChange}
-          />
+          {/* USERNAME */}
+          <div className="mt-4 flex w-full">
+            <div className="flex w-[80px] items-center justify-center rounded-l-lg bg-slate-700">
+              <FaUser className="text-[20px] text-white" />
+            </div>
+            <input
+              type="text"
+              placeholder="Username"
+              autoComplete="off"
+              required
+              name="name"
+              value={formData.name}
+              className=" w-full rounded-e-lg bg-slate-200 p-4 px-8 opacity-50 outline-none focus:opacity-100"
+              onChange={handleChange}
+            />
+          </div>
+          {/* EMAIL */}
+          <div className="flex w-full">
+            <div className="flex w-[80px] items-center justify-center rounded-l-lg bg-slate-700">
+              <IoMailSharp className="text-[24px] text-white" />
+            </div>
+            <input
+              type="email"
+              placeholder="Email"
+              autoComplete="off"
+              required
+              name="email"
+              value={formData.email}
+              className=" w-full rounded-e-lg bg-slate-200 p-4 px-8 opacity-50 outline-none focus:opacity-100"
+              onChange={handleChange}
+            />
+          </div>
+          {/* PASSWORD */}
+          <div className="flex w-full">
+            <div className="flex w-[80px] items-center justify-center rounded-s-lg bg-slate-700">
+              <FaLock className="text-[20px] text-white" />
+            </div>
+            <input
+              type="password"
+              placeholder="Password"
+              autoComplete="off"
+              required
+              name="password"
+              value={formData.password}
+              className=" w-full rounded-e-lg bg-slate-200 p-4 px-8 opacity-50 outline-none focus:opacity-100"
+              onChange={handleChange}
+            />
+          </div>
+
           {/* BUTTON */}
           <button
             disabled={loading}
-            className="w-full rounded-lg bg-slate-700 p-4 px-8 uppercase text-white hover:opacity-90 disabled:opacity-70 md:text-[20px]"
+            className="w-full rounded-lg bg-slate-800 p-4 px-8 uppercase text-white outline-1 outline-white hover:bg-slate-600 disabled:opacity-70 md:text-[20px]"
           >
             {loading ? "Loading..." : "Register"}
           </button>
         </form>
+        <button className="mt-4 w-full rounded-lg bg-red-900 p-4 px-8 uppercase text-white hover:bg-red-600 disabled:opacity-70 md:text-[20px]">
+          REGISTER WITH GOOGLE
+        </button>
         <div className="mt-5 flex w-full items-center justify-center">
-          <p>
+          <p className="text-[18px]">
             Have an account?{" "}
             <Link to="/login">
-              <span className="pl-2 text-textLink">Login</span>
+              <span className="pl-2 text-textLink font-semibold">Login</span>
             </Link>
           </p>
         </div>
--- FrontEnd/src/services/authServices.js
+++ FrontEnd/src/services/authServices.js
@@ -1,6 +1,6 @@
-const BASE_URL = `${import.meta.env.VITE_SERVER_URL}/auth/`;
+const BASE_URL = `${import.meta.env.VITE_SERVER_URL}/auth`;
 
-async function register(formData) {
+const signUp = async (formData) => {
   try {
     const res = await fetch(`${BASE_URL}/register`, {
       method: "POST",
@@ -10,10 +10,10 @@ async function register(formData) {
       body: JSON.stringify(formData),
     });
     const json = await res.json();
-    return json;
+    return json
   } catch (error) {
-    throw new Error(error);
+    throw new Error(error.message);
   }
-}
+};
 
-export { register };
+export { signUp };
