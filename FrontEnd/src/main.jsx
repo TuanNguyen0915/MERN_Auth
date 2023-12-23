534c469e32ed62911dcb19ceb8eb8e57026d9031
Tuan Nguyen <tuan.nat915@gmail.com>
2023-12-22 20:07:18 -0500

    add redux persist

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -3,21 +3,25 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { store } from "./redux/store.js";
-import {Provider} from 'react-redux'
+import { store, persistor } from "./redux/store.js";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter>
-      <ToastContainer
-        theme="dark"
-        position="top-right"
-        autoClose={2000}
-        pauseOnHover={false}
-        closeOnClick
-      />
-      <App />
-    </BrowserRouter>
+    <PersistGate persistor={persistor} loading={null}>
+      <BrowserRouter>
+        <ToastContainer
+          theme="dark"
+          position="top-right"
+          autoClose={2000}
+          pauseOnHover={false}
+          closeOnClick
+        />
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>,
 );
